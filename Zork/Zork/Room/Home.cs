d477436cac1173f21b9ae246e2fa8517152e8e21
Ahmad <Ahmad.a.ali@hotmail.com>
2017-09-25 18:10:46 +0200

    Massor med story implementerat för Ahmad. Men har fortfarande arbete att göra där, Även ett till rum är skapat och det är CLASSEN ODENPLAN!

--- Zork/Zork/Room/Home.cs
+++ Zork/Zork/Room/Home.cs
@@ -1,15 +1,36 @@
-﻿namespace Zork
+﻿using System;
+
+namespace Zork
 {
     public class Home : Room
     {
-        public Home()
+        public Home(CharacterIs character)
         {
             Name = "Home";
-            
-            Bio = "You are at home. " +
-                  "You can choose to go back to [bed] or [exit] through the [door], " +
-                  "but don't forget to pick your items you need for school.";
-            
+
+            //"You are at home. " +
+            //    "You can choose to go back to [bed] or [exit] through the [door], " +
+            //    "but don't forget to pick your items you need for school.";
+
+            if (character == CharacterIs.Ahmad)
+            {
+                Bio = "BEEEB!!! BEEEP!!! BEEEB!!! BEEEP!!!" +
+                      "Ugh! That damn alarm... SnOoooOooZZzzz..\n" +
+                      "";
+                
+                /* [Sound the Wakeup Alarm] [Sleep]
+                 * while loop, if wakeup !=0{"You snoozed once more"}else{"You woke up but still groggy"}
+                 
+                   [inspect] [pick] [exit]
+                   If inspect{" You see your stuff lying on the table, you should maybe take them with you"}
+                   If exit{"You put on your clothes and exit your house"*/
+            } else if (character == CharacterIs.Mimmi)
+            {
+                Bio = "";
+            }
+
+
+
             Exit = new[] {"Door", "Bed"};
             
         }
--- Zork/Zork/Stories/BusToSchool.cs
+++ Zork/Zork/Stories/BusToSchool.cs
@@ -2,10 +2,17 @@
 {
     public class BusToSchool: Stories
     {
-        public BusToSchool()
+        public BusToSchool(CharacterIs character)
         {
 
             Name = "Going from Bus to School";
+
+            if (character==CharacterIs.Ahmad)
+            {
+                Bio = "You enter the bus and its full of people." +
+                      "inspect{The bus is filled with people,}" +
+                      "";
+            }
             Bio =
                 "It has been an eventful morning, {0} takes a seat next to an older lady trying to catch a moment of rest?"; // CharacterIs;
 
