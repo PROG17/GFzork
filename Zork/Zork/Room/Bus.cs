f0dd7f74203585bc76da66bb548e5c8cd59f2e9a
Ahmad <Ahmad.a.ali@hotmail.com>
2017-09-26 15:36:46 +0200

    Ändringar del2 i storyn, missförstod hur vi skulle ha det så det blir lite extra fix där

--- Zork/Zork/Room/Bus.cs
+++ Zork/Zork/Room/Bus.cs
@@ -5,10 +5,10 @@
         public Bus()
         {
             Name = "Bus";
-            Bio = "The bus is packed with people" +
+            Bio = "The bus is packed with people"/* +
                   "Inspect the area, to find that there is an empty seat close to the door" +
                   "as you approach the seat you spot an older lady. [Sit] & [Give her your seat]." +
-                  "if sit is chosen, you get to rest and check your inventory but you wont be the first to exit the bus";
+                  "if sit is chosen, you get to rest and check your inventory but you wont be the first to exit the bus"*/;
             isLocked = true;
             
             Money money = new Money();
